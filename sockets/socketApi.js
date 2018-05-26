dbffb4d49e975c49231478f23d4165e0689f863d
Vivek Pandey (EJ, Vek) <vpandey@ramapo.edu>
2018-05-26 16:44:13 -0400

    Group chat is functional

--- sockets/socketApi.js
+++ sockets/socketApi.js
@@ -4,15 +4,32 @@ var io = socket_io();
 var socketApi = {};
 socketApi.io = io;
 
-io.on('connection', function(socket){
-    console.log("A user connected");
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
+let numberOfActiveSockets = 0;
+
+io.on('connection', (socket) => {
+    //Increment the count of alive connections on every new connection
+    numberOfActiveSockets++;
+    socket.broadcast.emit('news', socket.id + " has just connected.");
+    io.emit('numOfActiveConnections', numberOfActiveSockets);
+
+    //Upon a socket disconnect, decrement the count of alive connections
+    socket.on('disconnect', () => {
+        numberOfActiveSockets--;
+        socket.broadcast.emit('news', socket.id + " has just disconnected.");
+        io.emit('numOfActiveConnections', numberOfActiveSockets);
+    });
+    
+    //socket.emit('news', "Socket " + socket.id + " has an active connection");
+    socket.on('other-event', (data) => {
         console.log(data);
+    });
+
+    socket.on('user-message', (data) => {
+        io.emit('message', '<b>' + socket.id + ': </b>' + data);
     });
 });
 
-socketApi.sendNotification = function() {
+socketApi.sendNotification = () => {
     io.sockets.emit('hello', {msg: 'Hello World!'});
 }
 
