75f639755082aec8bcd840c778900388a8139cad
Vivek Pandey (EJ, Vek) <vpandey@ramapo.edu>
2018-05-27 13:16:17 -0400

    private messaging is functional now

--- sockets/socketApi.js
+++ sockets/socketApi.js
@@ -12,21 +12,35 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('news', socket.id + " has just connected.");
     io.emit('numOfActiveConnections', numberOfActiveSockets);
 
-    //Upon a socket disconnect, decrement the count of alive connections
+    //Upon a socket disconnect, 
+    //decrement the count of alive connections, emit that the socket left and new number of active sockets to everyone remaining
     socket.on('disconnect', () => {
         numberOfActiveSockets--;
+        //Emitting to all the connected sockets other than itself
         socket.broadcast.emit('news', socket.id + " has just disconnected.");
+        
+        //Emitting to all the connected sockets
         io.emit('numOfActiveConnections', numberOfActiveSockets);
     });
     
-    //socket.emit('news', "Socket " + socket.id + " has an active connection");
     socket.on('other-event', (data) => {
         console.log(data);
     });
 
-    socket.on('user-message', (data) => {
-        io.emit('message', '<b>' + socket.id + ': </b>' + data);
+    //Fulfilling request to send a private message
+    socket.on('private-message', (receipientSocketId, msg) => {
+        //Emitting to itself
+        socket.emit('message', '<b>' + socket.id + ': </b>' + msg);
+        //Emitting to the receipient socket
+        socket.broadcast.to(receipientSocketId).emit('message', '<b>' + socket.id + ': </b>' + msg);
     });
+
+    //Fulfilling reques to send a public message
+    socket.on('public-message', (msg) => {
+        //Emitting to all the connected sockets including itself
+        io.emit('message', '<b>' + socket.id + ': </b>' + msg);
+    });
+    //io.emit and io.sockets.emit are two same things
 });
 
 socketApi.sendNotification = () => {
